587741216fb13f52bfb68a3e6f9254a6de7701ed
riyan <riyan@riyan.home>
2017-04-17 22:31:03 +0100

    new line coded

--- src/main/java/BaseMain.java
+++ src/main/java/BaseMain.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 /**
  * Created by Sudhakar on 25/03/2016.
  */
-// Updated new line 
+// to merge newCode : 22:30 
 public class BaseMain {
     static WebDriver driver;
     public void openBrowser() throws IOException {
